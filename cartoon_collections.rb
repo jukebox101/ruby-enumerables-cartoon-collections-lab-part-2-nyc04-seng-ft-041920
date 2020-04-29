16347bc71dab80436567caa346551c4321660a78
Leonora Squires <leonora.squires@gmail.com>
2020-04-29 16:55:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find?(ifnone = nil){ |element|
   i = 0
-    while element != valid_calls[i] do
+    while element != valid_call[i] do
       i++
     end
   }
